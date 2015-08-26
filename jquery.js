27863772e10c487f3610b9ce6493746c276ee48b
bartas <bartekkubala@wp.pl>
2015-08-26 15:57:26 +0200

    second commit

--- jquery.js
+++ jquery.js
@@ -1,29 +1,32 @@
 $(document).ready(function() {
-    $('#button').click(function() {
+    $('.btn-success').click(function() {
         var toAdd = $('input[name=checkListItem]').val();
-        var pic = $('.picture').addClass('show');
-        $('.item').append("<li>" + toAdd + "</li>").css({
+        // var pic = $('.picture').addClass('show');
+        $('.item').append("<li>" + "&nbsp; &nbsp" + toAdd + "</li>").css({
             'background': 'rgb(73, 73, 73)',
             'margin-top': '5px',
             'display': 'block',
             'color': 'white',
+            'margin-left': '5px'
         });
     });
-    // $(document).on('click', 'li', function() {
-    //     $(this).slideUp(600).text('skasowany').css({
-    //         'float': 'right',
-    //         'color': 'black',
-    //     });
-    // });
-    $(document).on('click', 'li', function() {
-      $(this).css('background-color', 'rgb(36, 176, 73)');
+    $(document).on('dblclick', 'li', function() {
+        $(this).slideUp(1000).text('removed').css({
+            'float': 'right',
+            'color': 'black',
+            'background-color': 'white'
+        });
     });
-
+    $(document).on('click', 'li', function() {
+        $(this).css({
+            'background-color': '#d9534f',
+        });
+    });
     $('.clear').click(function() {
         $('.form-control').val();
         if (confirm('Are you sure?')) {
             $('li').remove();
-            $('.list').append('Lista wyczyszczona').fadeOut(1500);
+            $('.list').append('Lista wyczyszczona').fadeOut(600);
             document.location.reload();
         }
     });
