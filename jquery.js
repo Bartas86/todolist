da031004aca92696d2f646fbcdf1c2ac88f30119
bartas <bartekkubala@wp.pl>
2015-08-27 16:49:27 +0200

    Changes in all files

--- jquery.js
+++ jquery.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $('.btn-success').click(function() {
         var toAdd = $('input[name=checkListItem]').val();
-        // var pic = $('.picture').addClass('show');
-        $('.item').append("<li>" + "&nbsp; &nbsp" + toAdd + "</li>").css({
+        // Adding a new li on the list
+        $('.item').append("<li>" + "<span>" + toAdd + "</span>" + "</li>").css({
             'background': 'rgb(73, 73, 73)',
             'margin-top': '5px',
             'display': 'block',
@@ -10,6 +10,7 @@ $(document).ready(function() {
             'margin-left': '5px'
         });
     });
+    // Double click to remove li
     $(document).on('dblclick', 'li', function() {
         $(this).slideUp(1000).text('removed').css({
             'float': 'right',
@@ -17,11 +18,18 @@ $(document).ready(function() {
             'background-color': 'white'
         });
     });
+    // 1 click to highlight
     $(document).on('click', 'li', function() {
         $(this).css({
-            'background-color': '#d9534f',
+            'background-color': 'rgb(92, 184, 92)',
         });
+        if (!$(this).find('#done1').length) {
+          $(this).append('<span id="done1">Done</span>').fadeIn(1000);
+        }
+
+
     });
+    // Clear All data from the list
     $('.clear').click(function() {
         $('.form-control').val();
         if (confirm('Are you sure?')) {
@@ -30,6 +38,7 @@ $(document).ready(function() {
             document.location.reload();
         }
     });
+    // Clear input on a click
     $('.form-control').focus(function() {
         $('.form-control').val("");
     });
