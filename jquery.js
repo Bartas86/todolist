c0002c2beb55d5f41c4ab377fff04cb82a754ddf
bartas <bartekkubala@wp.pl>
2015-09-08 16:27:57 +0200

    Add some stuff

--- jquery.js
+++ jquery.js
@@ -3,16 +3,18 @@ $(document).ready(function() {
         var toAdd = $('input[name=checkListItem]').val();
         // Adding a new li on the list
         $('.item').append("<li>" + "<span>" + toAdd + "</span>" + "</li>").css({
-            'background': 'rgb(73, 73, 73)',
             'margin-top': '5px',
             'display': 'block',
             'color': 'white',
             'margin-left': '5px'
         });
+
+
     });
     // Double click to remove li
     $(document).on('dblclick', 'li', function() {
-        $(this).slideUp(1000).text('removed').css({
+        $(this).slideUp();
+        $(this).append('#done1').text('removed').css({
             'float': 'right',
             'color': 'black',
             'background-color': 'white'
@@ -23,8 +25,9 @@ $(document).ready(function() {
         $(this).css({
             'background-color': 'rgb(92, 184, 92)',
         });
+    // click to FINISH
         if (!$(this).find('#done1').length) {
-          $(this).append('<span id="done1">Done</span>').fadeIn(1000);
+          $(this).append('<span id="done1">Finished</span>').fadeIn(1000).css('margin-right', '17px');
         }
 
 
